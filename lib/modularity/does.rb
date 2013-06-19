d1b22398faec1a41e2076127b282bd38117950ff
Sebastian Schleicher <sebastian.julius@gmail.com>
2013-06-19 17:39:08 +0200

    includes are working like magic now

--- lib/modularity/does.rb
+++ lib/modularity/does.rb
@@ -2,22 +2,24 @@ require 'modularity/inflector'
 require 'modularity/as_trait'
 
 module Modularity
-  module Does
+	module Does
 
-    def self.included(base)
-      base.extend ClassMethods
-    end
-    
-    module ClassMethods
-      def does(trait_name, *args)
-	      trait_name = "#{self.name}Traits::#{Modularity::Inflector.camelize(trait_name.to_s)}Trait"
-        trait = Modularity::Inflector.constantize(trait_name)
-        macro = trait.instance_variable_get("@trait_macro") or raise "Missing trait directive in #{trait_name}"
-        class_exec(*args, &macro)
-      end
-    end
+		def self.included(base)
+			base.extend ClassMethods
+		end
 
-  end
+		module ClassMethods
+			def does(trait_name, *args)
+				caller_path = File.dirname(caller[0].partition(":").first)
+				require "#{caller_path}/#{trait_name}_trait"
+				full_trait_name = "#{self.name}Traits::#{Modularity::Inflector.camelize(trait_name.to_s)}Trait"
+				trait = Modularity::Inflector.constantize(full_trait_name)
+				macro = trait.instance_variable_get("@trait_macro") or raise "Missing trait directive in #{trait_name}"
+				class_exec(*args, &macro)
+			end
+		end
+
+	end
 end
 
 Object.send :include, Modularity::Does
